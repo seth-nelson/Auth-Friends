48a6a28db02455a7ab1db4c874245c3e59f004bf
Seth Nelson <seth.nelson@outlook.com>
2020-04-26 13:57:15 -0700

    initialized project and added components for project setup

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className='home-header'>
+        <h1>Home</h1>
+      </div>
+        <div className='friends-list-container'>
+          <PrivateRoute path='home' component={FriendsList} />
+          <Route path='/login' component={Login} />
+        </div>
     </div>
   );
 }
